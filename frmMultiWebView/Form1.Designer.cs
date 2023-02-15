ca1ac4a0686757bc36490d47c7799930d2703fc9
Ian Lu <yangyi@pchome.tw>
2023-02-16 00:39:16 +0800

    加入專案檔案。

---
+++ frmMultiWebView/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace frmMultiWebView
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.webView22 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView22)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(12, 60);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(719, 1355);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // webView22
+            // 
+            this.webView22.AllowExternalDrop = true;
+            this.webView22.CreationProperties = null;
+            this.webView22.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView22.Location = new System.Drawing.Point(737, 60);
+            this.webView22.Name = "webView22";
+            this.webView22.Size = new System.Drawing.Size(760, 1355);
+            this.webView22.TabIndex = 1;
+            this.webView22.ZoomFactor = 1D;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 27);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 27);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "自動戰鬥";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2770, 1429);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.webView22);
+            this.Controls.Add(this.webView21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView22)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView22;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ frmMultiWebView/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmMultiWebView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+        async void init() {
+            webView21.CreationProperties =
+            new Microsoft.Web.WebView2.WinForms.CoreWebView2CreationProperties();
+            webView21.CreationProperties.UserDataFolder = System.IO.Path.Combine(
+                Application.StartupPath, $"webView1");
+            await webView21.EnsureCoreWebView2Async();
+            webView21.CoreWebView2.Navigate("https://swordgale.online/");
+            webView22.CreationProperties =
+            new Microsoft.Web.WebView2.WinForms.CoreWebView2CreationProperties();
+            webView22.CreationProperties.UserDataFolder = System.IO.Path.Combine(
+                Application.StartupPath, $"webView2");
+            await webView22.EnsureCoreWebView2Async();
+            webView22.CoreWebView2.Navigate("https://swordgale.online/");
+
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            var viewHeight = this.Height - 125;
+            this.webView21.Height = viewHeight;
+            this.webView22.Height = viewHeight;
+            var viewWidth = (this.Width - 25 * 2 - 5 * (2 - 1)) / 2;
+            this.webView21.Width = viewWidth;
+            webView22.Left = this.webView21.Left + this.webView21.Width + 5;
+            this.webView22.Width = viewWidth;
+        }
+    }
+}
